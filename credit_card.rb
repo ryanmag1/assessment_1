4e1d8c552d51608c66cf8ef2d7afbce64cefbb75
Ryan Magee <ryanmag1@yahoo.com>
2016-03-04 22:39:22 -0700

    Created a payment method to subtract payments from @balance
    [#115087093]

--- credit_card.rb
+++ credit_card.rb
@@ -1,12 +1,16 @@
-# This class will save the world!
+# Class CreditCard will save the world!
 class CreditCard
   attr_reader :balance
 
   def initialize
-    @balance = 0.00
+    @balance = 0.0
   end
 
   def charge(amount)
     @balance += amount
+  end
+
+  def payment(payment_amount)
+    @balance -= payment_amount
   end
 end
